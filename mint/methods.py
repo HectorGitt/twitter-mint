2573a7561fa957182c250fc829972eb9ec2849cc
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-07-03 22:33:12 +0100

    added email thread methods

--- mint/methods.py
+++ mint/methods.py
@@ -2,6 +2,9 @@ from .models import Project
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.http import urlencode
+import threading
+from django.core.mail import EmailMessage
+
 class methods():
     def __init__(self):
         pass
@@ -35,3 +38,20 @@ class methods():
         else:
             return format_html('<a class=" disabled btn btn-success" href="{}">Ended</a>', url)
         
+class EmailThread(threading.Thread):
+    def __init__(self, subject, html_content, recipient_list, sender):
+        self.subject = subject
+        self.recipient_list = recipient_list
+        self.html_content = html_content
+        self.sender = sender
+        threading.Thread.__init__(self)
+
+    def run(self):
+        msg = EmailMessage(self.subject, self.html_content, self.sender, self.recipient_list)
+        msg.content_subtype = 'html'
+        msg.send()
+        print('sent')
+
+
+def send_html_mail(subject, from_email,plain_message, recipient_list, html_message, fail_silently):
+    EmailThread(subject, html_message, recipient_list, from_email).start()
