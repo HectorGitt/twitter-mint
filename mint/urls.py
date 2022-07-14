1f13e5e7e6b28c4f0f36ecd384c66107dacf8bf5
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-07-14 02:29:04 +0100

    added static file path

--- mint/urls.py
+++ mint/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
+
 from .views import ProjectListView
 
 
@@ -34,6 +36,10 @@ urlpatterns = [
     path('checkfollow/<uuid:project_id>', views.checkfollow, name='checkfollow'),
     path('checklike/<uuid:project_id>', views.checklike, name='checklike'),
     path('checkretweet/<uuid:project_id>', views.checkretweet, name='checkretweet'),
+    path('checkcomment/<uuid:project_id>', views.checkcomment, name='checkcomment'),
+    path('checkfollowers/<uuid:project_id>', views.checkfollowers, name='checkfollowers'),
+    path('checkmonths/<uuid:project_id>', views.checkmonths, name='checkmonths'),
     path('success', views.success, name='success'),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
