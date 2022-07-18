769842884407dcda1fd5c153a6c39778d9ffe949
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-07-18 16:16:51 +0100

    changed wallet id constraint

--- mint/models.py
+++ mint/models.py
@@ -102,8 +102,8 @@ class TwitterUser(models.Model):
     account_months = models.PositiveIntegerField(default=None, null=True, blank=True)
     projects = models.ManyToManyField(Project, blank=True, symmetrical=False, related_name='registered')
     email = models.CharField(max_length=255, null=True, blank=True)
-    eth_wallet_id = models.CharField(max_length=255, null=True, blank=True)
-    sol_wallet_id = models.CharField(max_length=255, null=True, blank=True)
+    eth_wallet_id = models.CharField(max_length=42, null=True, blank=True)
+    sol_wallet_id = models.CharField(max_length=44, null=True, blank=True)
     def __str__(self):
         return self.screen_name
 class EmailNotification(models.Model):
