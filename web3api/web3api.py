d787ef3e90770135812dd5ba2389dd9ad1a2e3af
dh_jae2ua <dh_jae2ua@ps638274.dreamhostps.com>
2022-08-03 15:57:31 -0700

    get infura key from .env

--- web3api/web3api.py
+++ web3api/web3api.py
@@ -1,7 +1,11 @@
-from web3.auto.infura import w3
+#from web3.auto.infura import w3
 from web3 import Web3
-class Web3():
+from decouple import config
+class Web3api():
+    def __init__(self):
+        self.http_url = 'https://mainnet.infura.io/v3/' + str(config('WEB3_INFURA_PROJECT_ID'))
     def web3eth_balance(self, wallet_addr):
+        w3 = Web3(Web3.HTTPProvider(self.http_url, request_kwargs={'timeout': 60}))
         value = w3.eth.get_balance(wallet_addr)
         balance =  value / (10**18)
         return balance
