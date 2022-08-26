e837222ebbae1b2f87bd41db1dc71496062dd200
HectorGitt <adeniyi.olaitanhector@yahoo.com>
2022-08-26 18:06:49 +0100

    added referral url

--- mint/urls.py
+++ mint/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path('project/<uuid:project_id>/check', views.checkactions, name='checkactions'),
     path('checkwalletbalance/<uuid:project_id>', views.checkwalletbalance, name='checkwalletbalance'),
     path('success', views.success, name='success'),
+    path('project/<uuid:project_id>/referral', views.request_referral_code, name='referral'),
 ]
 
 #added media upload root directory to url paths
