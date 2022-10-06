1c448c018a0f4dac1655f05b14819a14de58ed83
Adeniyi Olaitan <adeniyi.olaitanhector@yahoo.com>
2022-10-06 22:51:17 +0100

    Update settings.py
    changed static root path

--- twittermint/settings.py
+++ twittermint/settings.py
@@ -187,7 +187,7 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_URL = 'static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
